9e87377ff9ccc4a4caffa4d232f76ee97007b265
Vini1604 <vinicius.oliveira1998@hotmail.com>
2021-09-06 18:27:53 -0300

    criado um teste para o Git

---
+++ testeGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testeGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Isto é um teste para o Git");
+        }
+    }
+}
