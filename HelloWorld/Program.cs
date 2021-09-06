6d7af031753e284860eb0bcbfda190f2402c0537
Vini1604 <vinicius.oliveira1998@hotmail.com>
2021-09-06 18:15:32 -0300

    Criar um programa no VSCode

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Este é um programa teste p/ VS Code");
+        }
+    }
+}
